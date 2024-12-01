c9c0b6d606bb43ecba78f4dfc6d8b75b141a27f8
sohyun <sh0000@pusan.ac.kr>
2024-12-02 00:37:11 +0900

    feat: 상품 생성 시 응답 상태 코드 명시해주기

--- product/src/main/java/com/sparta/msa_exam/product/framework/web/controller/ProductCommandController.java
+++ product/src/main/java/com/sparta/msa_exam/product/framework/web/controller/ProductCommandController.java
@@ -5,9 +5,11 @@ import com.sparta.msa_exam.product.framework.web.dto.ProductCreateInputDTO;
 import com.sparta.msa_exam.product.framework.web.dto.ProductCreateOutputDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +19,7 @@ public class ProductCommandController {
 
     private final ProductUseCase productUseCase;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ProductCreateOutputDTO createProduct(
         @Valid @RequestBody ProductCreateInputDTO productCreateInputDTO
