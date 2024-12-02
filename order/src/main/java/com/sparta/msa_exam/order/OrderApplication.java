ae5397708f30d63017b1ac974344c609abdc5ffb
sohyun <sh0000@pusan.ac.kr>
2024-12-02 14:56:52 +0900

    feat: 필요한 어노테이션 추가

--- order/src/main/java/com/sparta/msa_exam/order/OrderApplication.java
+++ order/src/main/java/com/sparta/msa_exam/order/OrderApplication.java
@@ -3,9 +3,11 @@ package com.sparta.msa_exam.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = {"com.sparta.msa_exam.order", "com.sparta.common"})
 public class OrderApplication {
 
     public static void main(String[] args) {
