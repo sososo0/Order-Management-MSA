bea930f8bb43ddd8b6b0b2e9c3cfb3733b8645b5
sohyun <sh0000@pusan.ac.kr>
2024-12-02 21:07:11 +0900

    chore: auth 관련 설정 추가 - DB 연결 설정 및 의존성 추가 - spring security 설정 추가

--- auth/src/main/java/com/sparta/msa_exam/auth/AuthApplication.java
+++ auth/src/main/java/com/sparta/msa_exam/auth/AuthApplication.java
@@ -3,9 +3,11 @@ package com.sparta.msa_exam.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = {"com.sparta.msa_exam.auth", "com.sparta.common"})
 public class AuthApplication {
 
 	public static void main(String[] args) {
