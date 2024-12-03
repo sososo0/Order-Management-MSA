1a180553cc761b98b16c9cfe4b3991f4a7015732
sohyun <sh0000@pusan.ac.kr>
2024-12-03 22:49:26 +0900

    feat: 사용자 예외처리 추가

--- auth/src/main/java/com/sparta/msa_exam/auth/application/inputport/UserInputPort.java
+++ auth/src/main/java/com/sparta/msa_exam/auth/application/inputport/UserInputPort.java
@@ -1,10 +1,13 @@
 package com.sparta.msa_exam.auth.application.inputport;
 
+import com.sparta.common.exception.CustomException;
+import com.sparta.common.exception.ErrorCode;
 import com.sparta.msa_exam.auth.application.domain.User;
 import com.sparta.msa_exam.auth.application.domain.UserForAuthenticate;
 import com.sparta.msa_exam.auth.application.domain.UserForCreate;
 import com.sparta.msa_exam.auth.application.outputport.UserOutputPort;
 import com.sparta.msa_exam.auth.application.usecase.UserUseCase;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +22,7 @@ public class UserInputPort implements UserUseCase {
     @Override
     public User createUser(UserForCreate userForCreate) {
 
-        // TODO : 사용자 예외 처리(ex. 중복 처리 등등)
+        checkUsernameDuplication(userForCreate.username());
 
         String encodedPassword = passwordEncoder.encode(userForCreate.password());
 
@@ -29,15 +32,37 @@ public class UserInputPort implements UserUseCase {
     @Override
     public User authenticateUser(UserForAuthenticate userForAuthenticate) {
 
-        // TODO : 존재하지 않는 ID
-        User user = userOutputPort.findByUsername(userForAuthenticate.username())
-            .orElseThrow();
+        User user = validUsername(userForAuthenticate.username());
 
-        // TODO : 잘못된 비밀번호
         if (!passwordEncoder.matches(userForAuthenticate.password(), user.getPassword())) {
-            // TODO : 예외 처리
+            throw new CustomException(
+                ErrorCode.WRONG_PASSWORD.getCode(),
+                ErrorCode.WRONG_PASSWORD.getDescription(),
+                ErrorCode.WRONG_PASSWORD.getDetailMessage()
+            );
         }
 
+        return user;
+    }
+
+    private void checkUsernameDuplication(String username) {
+        Optional<User> user = userOutputPort.findByUsername(username);
+        if (user.isPresent()) {
+            throw new CustomException(
+                ErrorCode.USERNAME_ALREADY_EXIST.getCode(),
+                ErrorCode.USERNAME_ALREADY_EXIST.getDescription(),
+                ErrorCode.USERNAME_ALREADY_EXIST.getDetailMessage()
+            );
+        }
+    }
+
+    private User validUsername(String username) {
+        User user = userOutputPort.findByUsername(username)
+            .orElseThrow(() -> new CustomException(
+                ErrorCode.USER_NOT_EXIST.getCode(),
+                ErrorCode.USER_NOT_EXIST.getDescription(),
+                ErrorCode.USER_NOT_EXIST.getDetailMessage()
+            ));
         return user;
     }
 }
